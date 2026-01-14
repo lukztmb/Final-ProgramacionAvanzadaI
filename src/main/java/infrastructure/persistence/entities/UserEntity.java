c3c3703ec791a7c0f4dd7ef05b705727f4fc2112
JoacDc <delcantodiegojoaquin5@gmail.com>
2026-01-14 02:02:38 -0300

    Clases UserMapper, UserEntity, UserImp, IUserRepositori realizadas

--- src/main/java/infrastructure/persistence/entities/UserEntity.java
+++ src/main/java/infrastructure/persistence/entities/UserEntity.java
@@ -1,4 +1,65 @@
 package infrastructure.persistence.entities;
 
+import domain.model.UserStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users_table")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Column(nullable = false)
+    private String activationCode;
+
+    @Column(nullable = false)
+    private LocalDateTime activationExpiresAt;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderEntity> orders = new ArrayList<OrderEntity>();
+
+    public UserEntity() {}
+
+    public void addOrders(OrderEntity order){
+        orders.add(order);
+        order.setUser(this);
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public UserStatus getStatus() { return status; }
+    public String getActivationCode() { return activationCode; }
+    public LocalDateTime getActivationExpiresAt() { return activationExpiresAt; }
+    public void serActivationCode(String activationCode) { this.activationCode = activationCode; }
+    public void setExpiresAt(LocalDateTime expiresAt) { this.activationExpiresAt = expiresAt; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public List<OrderEntity> getOrders (){return orders;}
+    public void setOrders(List<OrderEntity> orders) {this.orders = orders;}
+
+
 }
---
+++ src/main/java/infrastructure/persistence/mapper/UserMapper.java
@@ -0,0 +1,49 @@
+package infrastructure.persistence.mapper;
+
+import domain.model.User;
+import domain.model.UserStatus;
+import infrastructure.persistence.entities.UserEntity;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
+import java.time.LocalDateTime;
+
+@Component
+public class UserMapper {
+
+    public User toUserDomain(UserEntity entity) {
+        if (entity == null) return null;
+        try {
+            Constructor<User> constructor = User.class.getDeclaredConstructor(
+                    Long.class,
+                    String.class,
+                    String.class,
+                    UserStatus.class,
+                    LocalDateTime.class
+            );
+            constructor.setAccessible(true);
+            User user = constructor.newInstance(
+                    entity.getId(),
+                    entity.getEmail(),
+                    entity.getPassword(),
+                    entity.getStatus(),
+                    entity.getCreatedAt()
+            );
+            return user;
+        }catch (Exception e){
+            throw new RuntimeException("Error Reconstruyendo el User desde la Persistencia",e);
+        }
+    }
+
+    public UserEntity toUserEntity(User user) {
+        if (user == null) return null;
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(user.getId());
+        userEntity.setEmail(user.getEmail());
+        userEntity.setPassword(user.getPassword());
+        userEntity.setStatus(user.getStatus());
+        userEntity.setCreatedAt(user.getCreatedAt());
+        return userEntity;
+    }
+
+}
